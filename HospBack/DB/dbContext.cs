263f22b28a3cc570f080dd1fadca4697c4f1e456
maxide201 <maxide201@ya.ru>
2021-11-16 17:51:20 +0300

    Доабавлена поддержка Identity

--- HospBack/DB/dbContext.cs
+++ HospBack/DB/dbContext.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -6,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace HospBack.DB
 {
-    public partial class dbContext : DbContext
+    public partial class dbContext : IdentityDbContext<User>
     {
         private readonly string _connectionString;
 
@@ -21,14 +23,7 @@ namespace HospBack.DB
         }
 
         public virtual DbSet<Analysis> Analyses { get; set; }
-        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
-        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
-        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-        public virtual DbSet<AspNetUserRole> AspNetUserRoles { get; set; }
-        public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
-        public virtual DbSet<Certificate> Certificates { get; set; }
+		public virtual DbSet<Certificate> Certificates { get; set; }
         public virtual DbSet<Doctor> Doctors { get; set; }
         public virtual DbSet<DoctorType> DoctorTypes { get; set; }
         public virtual DbSet<Hospital> Hospitals { get; set; }
@@ -46,6 +41,8 @@ namespace HospBack.DB
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
 
             modelBuilder.Entity<Analysis>(entity =>
@@ -68,99 +65,101 @@ namespace HospBack.DB
                     .IsRequired()
                     .HasColumnName("result_description");
 
-                entity.Property(e => e.UserId)
+                entity.Property(e => e.PatientId)
                     .IsRequired()
                     .HasColumnName("user_id");
 
                 entity.HasOne(d => d.Patient)
                     .WithMany(p => p.Analyses)
-                    .HasForeignKey(d => d.UserId)
+                    .HasForeignKey(d => d.PatientId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("analyse_user_id");
             });
 
-            modelBuilder.Entity<AspNetRole>(entity =>
-            {
-                entity.Property(e => e.Name).HasMaxLength(256);
+			modelBuilder.Entity<AspNetRole>(entity =>
+			{
+				entity.Property(e => e.Name).HasMaxLength(256);
 
-                entity.Property(e => e.NormalizedName).HasMaxLength(256);
-            });
+				entity.Property(e => e.NormalizedName).HasMaxLength(256);
+			});
 
-            modelBuilder.Entity<AspNetRoleClaim>(entity =>
-            {
-                entity.Property(e => e.RoleId).IsRequired();
+			modelBuilder.Entity<AspNetRoleClaim>(entity =>
+			{
+				entity.Property(e => e.RoleId).IsRequired();
 
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.AspNetRoleClaims)
-                    .HasForeignKey(d => d.RoleId);
-            });
+				entity.HasOne(d => d.Role)
+					.WithMany(p => p.AspNetRoleClaims)
+					.HasForeignKey(d => d.RoleId);
+			});
 
-            modelBuilder.Entity<AspNetUser>(entity =>
-            {
-                entity.ToTable("AspNetUser");
+			modelBuilder.Entity<AspNetUser>(entity =>
+			{
+				entity.ToTable("AspNetUser");
 
-                entity.Property(e => e.Discriminator).IsRequired();
+				entity.Property(e => e.Discriminator).IsRequired();
 
-                entity.Property(e => e.Email).HasMaxLength(256);
+				entity.Property(e => e.Email).HasMaxLength(256);
 
-                entity.Property(e => e.LockoutEnd).HasColumnType("timestamp with time zone");
+				entity.Property(e => e.LockoutEnd).HasColumnType("timestamp with time zone");
 
-                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+				entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
 
-                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+				entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
 
-                entity.Property(e => e.UserName).HasMaxLength(256);
-            });
+				entity.Property(e => e.UserName).HasMaxLength(256);
+			});
 
-            modelBuilder.Entity<AspNetUserClaim>(entity =>
-            {
-                entity.ToTable("AspNetUserClaim");
+			modelBuilder.Entity<AspNetUserClaim>(entity =>
+			{
+				entity.ToTable("AspNetUserClaim");
 
-                entity.Property(e => e.UserId).IsRequired();
+				entity.Property(e => e.UserId).IsRequired();
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserClaims)
-                    .HasForeignKey(d => d.UserId);
-            });
+				entity.HasOne(d => d.User)
+					.WithMany(p => p.AspNetUserClaims)
+					.HasForeignKey(d => d.UserId);
+			});
 
-            modelBuilder.Entity<AspNetUserLogin>(entity =>
-            {
-                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+			modelBuilder.Entity<AspNetUserLogin>(entity =>
+			{
+				entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
 
-                entity.ToTable("AspNetUserLogin");
+				entity.ToTable("AspNetUserLogin");
 
-                entity.Property(e => e.UserId).IsRequired();
+				entity.Property(e => e.UserId).IsRequired();
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserLogins)
-                    .HasForeignKey(d => d.UserId);
-            });
+				entity.HasOne(d => d.User)
+					.WithMany(p => p.AspNetUserLogins)
+					.HasForeignKey(d => d.UserId);
+			});
 
-            modelBuilder.Entity<AspNetUserRole>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.RoleId });
+			modelBuilder.Entity<AspNetUserRole>(entity =>
+			{
+				entity.HasKey(e => new { e.UserId, e.RoleId });
 
-                entity.ToTable("AspNetUserRole");
+				entity.ToTable("AspNetUserRole");
 
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.AspNetUserRoles)
-                    .HasForeignKey(d => d.RoleId);
+				entity.HasOne(d => d.Role)
+					.WithMany(p => p.AspNetUserRoles)
+					.HasForeignKey(d => d.RoleId);
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserRoles)
-                    .HasForeignKey(d => d.UserId);
-            });
+				entity.HasOne(d => d.User)
+					.WithMany(p => p.AspNetUserRoles)
+					.HasForeignKey(d => d.UserId);
+			});
 
-            modelBuilder.Entity<AspNetUserToken>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+			modelBuilder.Entity<AspNetUserToken>(entity =>
+			{
+				entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.AspNetUserTokens)
-                    .HasForeignKey(d => d.UserId);
-            });
+				entity.HasOne(d => d.User)
+					.WithMany(p => p.AspNetUserTokens)
+					.HasForeignKey(d => d.UserId);
+			});
 
-            modelBuilder.Entity<Certificate>(entity =>
+            /////////////////////////////////
+
+			modelBuilder.Entity<Certificate>(entity =>
             {
                 entity.ToTable("certificates");
 
@@ -294,7 +293,7 @@ namespace HospBack.DB
                     .HasColumnName("phone_number");
 
                 entity.Property(e => e.Email)
-                    .HasColumnName("doctor_surname");
+                    .HasColumnName("email");
             });
 
             modelBuilder.Entity<Referral>(entity =>
@@ -373,7 +372,7 @@ namespace HospBack.DB
                     .IsRequired()
                     .HasColumnName("status");
 
-                entity.Property(e => e.UserId)
+                entity.Property(e => e.PatientId)
                     .IsRequired()
                     .HasColumnName("user_id");
 
@@ -387,7 +386,7 @@ namespace HospBack.DB
 
                 entity.HasOne(d => d.Patient)
                     .WithMany(p => p.Visits)
-                    .HasForeignKey(d => d.UserId)
+                    .HasForeignKey(d => d.PatientId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("visit_user_id");
             });
