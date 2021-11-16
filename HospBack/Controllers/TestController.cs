75bead57faa99426e7849090f98827bb5eb7e1ab
maxide201 <maxide201@ya.ru>
2021-11-16 17:52:39 +0300

    Контроллер для теста и инициализации начальных данных в бд

--- HospBack/Controllers/TestController.cs
+++ HospBack/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-﻿using HospBack.DB.Helpers;
+﻿using HospBack.DB;
+using HospBack.DB.Helpers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -9,9 +11,18 @@ using System.Threading.Tasks;
 
 namespace HospBack.Controllers
 {
-	public class TestController : ControllerBase
+    
+    public class TestController : ControllerBase
 	{
-        public TestController(IConfiguration configuration) : base(configuration) { }
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public TestController(IConfiguration configuration, UserManager<User> userManager,
+                                    RoleManager<IdentityRole> roleManager) : base(configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
 
         [HttpGet]
         public JsonResult Index()
@@ -38,10 +49,43 @@ namespace HospBack.Controllers
                     return Message(connected ? "Connected" : "Not connected");
                 }
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 return Message("Exeption thrown");
             }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Init()
+		{
+            string[] roles = new string[] { "admin", "doctor", "registrar" };
+
+            foreach (string role in roles)
+            {
+                var isExist = await _roleManager.RoleExistsAsync(role);
+                var roleModel = new Role();
+                roleModel.Name = role;
+                if (!isExist)
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+            }
+            var user = new User
+            {
+                UserName = "admin"
+            };
+
+            var admin = await _userManager.FindByNameAsync(user.UserName);
+            if (admin == null)
+            {
+                var password = new PasswordHasher<User>();
+                var hashed = password.HashPassword(user, "secret");
+                user.PasswordHash = hashed;
+
+                await _userManager.CreateAsync(user);
+                await _userManager.AddToRoleAsync(user, "admin");
+
+            }
+
+            return new JsonResult("success!");
         }
 
         #region Private methods
