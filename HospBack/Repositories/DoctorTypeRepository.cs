1276bc165a616fe72c34f6adc0a6d36ed1ac25e3
maxide201 <maxide201@ya.ru>
2021-11-19 13:31:36 +0300

    DoctorType Repository/Service

---
+++ HospBack/Repositories/DoctorTypeRepository.cs
@@ -0,0 +1,39 @@
+﻿using HospBack.DB;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospBack.Repositories
+{
+	public interface IDoctorTypeRepository
+	{
+		List<DoctorType> GetDoctorTypes(dbContext ctx);
+		void CreateDoctorType(dbContext ctx, DoctorType doctorType);
+		void UpdateDoctorType(dbContext ctx, DoctorType doctorType);
+		DoctorType GetDoctorTypeById(dbContext ctx, int id);
+		DoctorType GetDoctorTypeByType(dbContext ctx, string type);
+	}
+	public class DoctorTypeRepository : IDoctorTypeRepository
+	{
+		public List<DoctorType> GetDoctorTypes(dbContext ctx) =>
+			ctx.DoctorTypes.ToList();
+
+		public void CreateDoctorType(dbContext ctx, DoctorType doctorType) =>
+			ctx.DoctorTypes.Add(doctorType);
+
+		public void UpdateDoctorType(dbContext ctx, DoctorType doctorType) =>
+			ctx.DoctorTypes.Update(doctorType);
+
+		public DoctorType GetDoctorTypeById(dbContext ctx, int id) =>
+			ctx.DoctorTypes.Where(x => x.Id == id).FirstOrDefault();
+
+		public DoctorType GetDoctorTypeByType(dbContext ctx, string type) =>
+			ctx.DoctorTypes.Where(x => x.Type == type).FirstOrDefault();
+
+		
+
+	}
+}
---
+++ HospBack/Services/DoctorTypeService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HospBack.DB;
+using HospBack.Exceptions;
+using HospBack.Repositories;
+
+namespace HospBack.Services
+{
+	public interface IDoctorTypeService
+	{
+		List<DoctorType> GetAllDoctorTypes(dbContext ctx);
+		void CreateDoctorType(dbContext ctx, DoctorType doctorType);
+		void EditDoctorType(dbContext ctx, DoctorType doctorType);
+		void DeleteDoctorType(dbContext ctx, int id);
+	}
+	public class DoctorTypeService : IDoctorTypeService
+	{
+		IDoctorTypeRepository _doctorTypeRepository;
+		public DoctorTypeService(IDoctorTypeRepository doctorTypeRepository)
+		{
+			_doctorTypeRepository = doctorTypeRepository;
+		}
+
+		public void CreateDoctorType(dbContext ctx, DoctorType doctorType)
+		{
+			isDataCorrect(doctorType);
+
+			var model = _doctorTypeRepository.GetDoctorTypeByType(ctx, doctorType.Type);
+			if (model != null)
+				throw new ModelAlreadyExistException();
+
+			_doctorTypeRepository.CreateDoctorType(ctx, doctorType);
+		}
+
+		public void DeleteDoctorType(dbContext ctx, int id)
+		{
+			var model = _doctorTypeRepository.GetDoctorTypeById(ctx, id);
+			if (model == null)
+				throw new ModelNotExistException();
+
+			//_doctorTypeRepository.Delete(ctx, id);
+		}
+
+		public void EditDoctorType(dbContext ctx, DoctorType doctorType)
+		{
+			isDataCorrect(doctorType);
+
+			var model = _doctorTypeRepository.GetDoctorTypeById(ctx, doctorType.Id);
+			if (model == null)
+				throw new ModelNotExistException();
+
+			_doctorTypeRepository.UpdateDoctorType(ctx, doctorType);
+		}
+
+		public List<DoctorType> GetAllDoctorTypes(dbContext ctx)
+		{
+			return _doctorTypeRepository.GetDoctorTypes(ctx);
+		}
+
+
+		public bool isDataCorrect(DoctorType doctorType)
+		{
+			if (doctorType?.Type != null)
+				return true;
+			throw new IncorrectDataException();
+		}
+	}
+}
