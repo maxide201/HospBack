9e7e9ab404565dbbc24298d1c2be68f53dd582b9
maxide201 <maxide201@ya.ru>
2021-11-19 13:17:49 +0300

    Реализация DDD Hospital

--- HospBack/Controllers/HospitalController.cs
+++ HospBack/Controllers/HospitalController.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using HospBack.DB.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HospBack.Services;
+using Microsoft.Extensions.DependencyInjection;
+using HospBack.DB;
 
 namespace HospBack.Controllers
 {
-	public class HospitalController
-	{
-	}
+    [Route("[controller]")]
+    public class HospitalController : ControllerBase
+    {
+        IHospitalService _hospitalService;
+        public HospitalController(IConfiguration configuration, IHospitalService hospitalService) : base(configuration) 
+        {
+            _hospitalService = hospitalService;
+        }
+
+        [HttpGet]
+        [Route("hospitals")]
+        public IActionResult GetAllHospitals()
+        {
+            using (var ctx = CreateDataContext())
+			{
+                var hospitals = _hospitalService.GetAllHospitals(ctx);
+                return View(hospitals);
+            }
+        }
+    }
 }
---
+++ HospBack/Services/HospitalService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HospBack.DB;
+using HospBack.Exceptions;
+using HospBack.Repositories;
+
+namespace HospBack.Services
+{
+	public interface IHospitalService
+	{
+		List<Hospital> GetAllHospitals(dbContext ctx);
+		void CreateHospital(dbContext ctx, Hospital hospital);
+		void EditHospital(dbContext ctx, Hospital hospital);
+		void DeleteHospital(dbContext ctx, int id);
+	}
+	public class HospitalService : IHospitalService
+	{
+		IHospitalRepository _hospitalRepository;
+		public HospitalService(IHospitalRepository hospitalRepository)
+		{
+			_hospitalRepository = hospitalRepository;
+		}
+
+		public void CreateHospital(dbContext ctx, Hospital hospital)
+		{
+			isDataCorrect(hospital);
+
+			var model = _hospitalRepository.GetHospitalByName(ctx, hospital.Name);
+			if (model != null)
+				throw new ModelAlreadyExistException();
+
+			_hospitalRepository.CreateHospital(ctx, hospital);
+		}
+
+		public void DeleteHospital(dbContext ctx, int id)
+		{
+			var model = _hospitalRepository.GetHospitalById(ctx, id);
+			if (model == null)
+				throw new ModelNotExistException();
+
+			//_hospitalRepository.Delete(ctx, id);
+		}
+
+		public void EditHospital(dbContext ctx, Hospital hospital)
+		{
+			isDataCorrect(hospital);
+
+			var model = _hospitalRepository.GetHospitalById(ctx, hospital.Id);
+			if (model == null)
+				throw new ModelNotExistException();
+
+			_hospitalRepository.UpdateHospital(ctx, hospital);
+		}
+
+		public List<Hospital> GetAllHospitals(dbContext ctx)
+		{
+			return _hospitalRepository.GetHospitals(ctx);
+		}
+
+
+		public bool isDataCorrect(Hospital hospital)
+		{
+			if (hospital?.Name != null && hospital?.Address != null && hospital?.PhoneNumber != null)
+				return true;
+			throw new IncorrectDataException();
+		}
+	}
+}
