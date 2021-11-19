1acbdce9581c458b8e1a84c79ea842c8a7b4fe14
maxide201 <maxide201@ya.ru>
2021-11-19 13:49:49 +0300

    Собственные Exception

---
+++ HospBack/Exceptions/ModelNotExistException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospBack.Exceptions
+{
+	public class ModelNotExistException : Exception
+	{
+	}
+}
